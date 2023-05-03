a8897d7b248fdaea546a62428abeb275735f64ce
cpasupala <cpasupala@gmail.com>
2023-05-03 11:22:52 -0700

    added rounds

--- newmain.py
+++ newmain.py
@@ -10,14 +10,16 @@ class Board:
     __lastplayed = 'x'
     __debug=True
     __kholoc = []
-    def __init__(self,boardlen=10,os = 'x'):
+    rounds = np.array([])
+    def __init__(self,boardlen=10,os = 'x',debug=False):
         self.blen = boardlen
         while ((self.blen <= 10) | (self.blen > 30)):
             self.blen = int(input ("A good board length is between 11 and 30: "))
         self.barr = np.array((' '.join('x'*5+'_'*(self.blen-10)+'o'*5)).split(' '))
         self.offside = os
-        self.debug=True
+        self.debug=debug
         self.kholoc = []
+        self.rounds = np.array([])
         return
 
     def get_board(self,sep=''):
@@ -38,10 +40,20 @@ class Board:
         printstr = ''
         if(self.debug):
             printstr += f'offside is {offside}: board is [{self.barr}]: {randrock}->{randplace} ->newboard '
+        prevarr = self.barr
         self.barr[[randrock,randplace]] = self.barr[[randplace,randrock]]
+        if (len(self.rounds)):
+            self.rounds = np.vstack((self.rounds,np.concatenate((prevarr,self.barr))))
+        else:
+            self.rounds = np.concatenate((prevarr,self.barr))
+
         if(self.debug):
             printstr += f'[{self.barr}]'
             print(printstr)
+        return
+
+    def dump_rounds (self):
+        print(self.rounds.shape)
         return
 
     def board_validate(self):
@@ -107,21 +119,30 @@ if __name__ == "__main__":
                 os = input("Input offside : ")
                 print(f'Cleared Board: {validate(bstr,os)}')
     else:
-        b = Board(14,'x')
-        board_str = b.get_board(sep=' ')
-        print(f'Board initialized to : {board_str}')
-        print(f'The offensive side   : {b.offside}')
+        no_games = 2
+        xwins = 0
+        owins = 0
+        for i in range(no_games):
+            b = Board(14,'x')
+            board_str = b.get_board(sep=' ')
+            #print(f'Board initialized to : {board_str}')
+            #print(f'The offensive side   : {b.offside}')
 
-        game_on = True
-        while(game_on):
-            b.random_play('x')
-            game_on = b.board_validate()
-            if(not game_on):
-                print('x wins the game')
-                break
-            b.random_play('o')
-            game_on = b.board_validate()
-            if(not game_on):
-                print('o wins the game')
-                break
+            game_on = True
+            while(game_on):
+                b.random_play('x')
+                game_on = b.board_validate()
+                if(not game_on):
+                    #print('x wins the game')
+                    b.dump_rounds()
+                    xwins +=1
+                    break
+                b.random_play('o')
+                game_on = b.board_validate()
+                if(not game_on):
+                    #print('o wins the game')
+                    b.dump_rounds()
+                    owins +=1
+                    break
+        print(f'x wins {(xwins/no_games)*100:.1f}% of {no_games } played')
 
