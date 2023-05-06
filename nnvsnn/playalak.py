155ea15e3cdce715a4c9fea25f92f7491a964f31
cpasupala <cpasupala@gmail.com>
2023-05-06 12:13:32 -0700

    new adds

---
+++ nnvsnn/playalak.py
@@ -0,0 +1,368 @@
+import random
+import re
+import numpy as np
+import colorama
+from colorama import Fore, Style
+import pickle
+import pandas as pd
+from pathlib import Path
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+class NNClass:
+    model = MLPClassifier
+    nnrock = 'x'
+    mname = ''
+    def __init__(self,rock,mname):
+        self.mname = mname
+        self.nnrock = rock
+        if Path(mname).is_file():
+            self.model = pickle.load(open(mname,"rb"))
+        else:
+            raise Exception("Could not find the model. Exiting")
+        return
+
+    def nnplay (self,board,rock,kholocs):
+        r_idx = np.argwhere(board==rock).reshape(1,-1)[0]
+        if (len(r_idx)==0):
+            raise Exception(Fore.RED+"There are no rocks and still playing. This should  not happen!")
+
+        b_idx = np.argwhere(board=='_').reshape(1,-1)[0]
+        if (len(b_idx)==0):
+            raise Exception(Fore.RED+"No place on the board ? You must be kidding me!")
+        X = []
+        rx = []
+        for r in r_idx:
+            for x in b_idx:
+                temp_board = np.array(board)
+                temp_board[[r,x]] = temp_board[[x,r]]
+                if(not detect_suicide(temp_board,rock)):
+                    rx.append((r,x))
+                    X.append(np.concatenate((board,temp_board)))
+        X = np.array(X)
+
+        opponent = 'x'
+        if (rock == 'x') :
+            opponent = 'o'
+        else:
+            opponent = 'x'
+
+        X = np.where(X==rock,1, X)
+        X = np.where(X=='_',0,X)
+        X = np.where(X==opponent,-1,X)
+        X = np.array(X,dtype=float)
+
+        if(self.mname == "perfect_model_80.pkl"):
+            probarray = self.model.predict_proba(X)[:,1]
+        else:
+            probarray = self.model.predict_proba(X)[:,3:].sum(axis=1)
+        #probarray = self.model.predict_proba(X)[:,6]
+        #print(rx)
+        #print(probarray)
+        #print(f'choosing {rx[np.argmax(probarray)]}')
+        return (rx[np.argmax(probarray)])
+        
+
+class Board:
+
+    blen = 14
+    barr = []
+    nn_rock ='x'
+    rand_rock = 'o'
+    err_code = 0
+    kholoc = []
+    debug = False
+    showplay = True
+    rounds =[]
+    gain = []
+    deadlock = False
+    
+    def __init__(self,blen,toss):
+        self.blen = blen
+        if (toss):
+            nn_rock = 'x'
+            rand_rock = 'o'
+        else:
+            nn_rock = 'o'
+            rand_rock = 'x'
+        self.barr = np.array((' '.join('x'*5+'_'*(blen-10)+'o'*5)).split(' '))
+        self.nn_rock = 'x'
+        self.rand_rock = 'o'
+        self.err_code = 0
+        self.kholoc = []
+        self.debug = False
+        self.showplay = True 
+        self.suicide_in_game = False
+        self.rounds = np.array([])
+        self.gain = []
+        self.deadlock = False
+        return
+
+    def detect_suicide(self, board, rock):
+        s = ''.join(board)
+        if (rock == 'x'):
+            kill = re.compile('(?=(xo+x))')
+            suicide = re.compile('(?=(ox+o))')
+        else:
+            kill = re.compile('(?=(ox+o))')
+            suicide = re.compile('(?=(xo+x))')
+
+        for m in re.finditer(kill,s):
+            board[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+        s = ''.join(board)
+        for m in re.finditer(suicide,s):
+            board[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+            return True
+
+        return False
+
+    def validate(self,rock):
+        self.kholoc = []
+        opponent = ''
+        gain = 0
+        if (rock == 'x'):
+        # Parse for the kill and then for suicide
+            kill = re.compile('(?=(xo+x))')
+            suicide = re.compile('(?=(ox+o))')
+            opponent = 'o'
+        else:
+            kill = re.compile('(?=(ox+o))')
+            suicide = re.compile('(?=(xo+x))')
+            opponent = 'x'
+
+        s = ''.join(self.barr)
+        for m in re.finditer(kill,s):
+            if(self.debug):
+                print(f'board_validate: Detected kill at {m.start(1)}:{m.end(1)}')
+            self.barr[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+            gain += m.end(1)-m.start(1)-2
+            if((m.end(1)-m.start(1)) == 5):
+                # We got a kill here
+                self.kholoc.append(m.start(1)+1)
+        s = ''.join(self.barr)
+        for m in re.finditer(suicide,s):
+            if(self.debug):
+                print(f'board_validate: Detected suicide at {m.start(1)}:{m.end(1)}')
+            self.barr[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+            gain -= m.end(1)-m.start(1)-2
+        self.gain.append(gain)
+            
+        if(len(np.argwhere(self.barr == opponent)) >1):
+            if (len(self.rounds) > 500):
+                self.deadlock = True
+                return False
+            return True
+        else:
+            return False
+
+    def play_n_validate(self,playfunc,rock):
+        '''
+        Get all possible valid moves for the rock. call the function given by 
+        the user. The function 'playfunc' should return the from-to combination
+        of indices.
+        board verifies if it is a legal move and then updates the board.
+        '''
+        r_idx = np.argwhere(self.barr==rock).reshape(1,-1)[0]
+        if (len(r_idx)==0):
+            raise Exception(Fore.RED+"There are no rocks and still playing. Bad Kitty!")
+            self.err_code = -1
+            return False
+
+        b_idx = np.argwhere(self.barr=='_').reshape(1,-1)[0]
+        if (len(b_idx)==0):
+            raise Exception(Fore.RED+"No place on the board ? You must be kidding me!")
+            self.err_code = -2
+            return False
+        
+        '''
+        Kho is allowed. So, we dont exclude those locations. That is the 
+        reason for commenting out this code.
+
+        # Exclude kho locations
+        b_idx = np.delete(b_idx, np.isin(b_idx,self.kholoc))
+        if (len(b_idx)==0):
+            print(Fore.RED+"No place on the board after remvoing Kho locations? ")
+            self.err_code = -3
+            return False
+        '''
+        
+        randrock = 0
+        suicide_list = []
+        for randrock in r_idx:
+            for x in b_idx:
+                temp_board = np.array(self.barr)
+                temp_board[[randrock,x]] = temp_board[[x,randrock]]
+                if(self.detect_suicide(temp_board,rock)):
+                    suicide_list.append((randrock,x))
+
+        if(self.debug):
+            print(f'legal moves: {b_idx}')
+            print(f'suicide moves: {suicide_list}')
+        # Now, we know all the legal moves and its subset non-suicide legal moves
+        # See what the user function returns
+        u_rock,u_place = playfunc(np.array(self.barr),rock,self.kholoc)
+        if(self.debug):
+            print(f'{rock} moved {u_rock}->{u_place}')
+
+        if(np.isin(u_rock,r_idx) and np.isin(u_place,b_idx)):
+            prevboard = np.array(self.barr)
+            if (self.showplay):
+                bstr = ''.join(self.barr)
+                printstr = f'Round:{len(self.rounds)}:{bstr} [{rock}:{u_rock}->{u_place}] '
+            self.barr[[u_rock,u_place]] = self.barr[[u_place,u_rock]]
+            if(len(self.rounds)):
+                self.rounds = np.vstack((self.rounds,np.concatenate((prevboard,self.barr))))
+            else:
+                self.rounds = np.concatenate((prevboard,self.barr))
+
+            if(self.showplay):
+                bstr = ''.join(self.barr)
+                print(printstr+f'{bstr}')
+            if(len(suicide_list)>0):
+                if ((u_rock,u_place) in suicide_list):
+                    print(Fore.RED+f'Warning: Player {rock} made a suicide move. Ignoring')
+                    print(Style.RESET_ALL)
+                    self.suicide_in_game = True
+            # Clear Board and check for victory !!
+            if(self.validate(rock)):
+                return True
+            else:
+                self.err_code = -3
+                return False
+        else:
+            raise Exception (Fore.RED+f'Error: Player {rock} played an illegal move')
+            print(self.barr)
+            print(f'{rock}:{u_rock}-->{u_place}')
+            self.err_code = -4
+            return False
+
+manual = False
+def randplay(board,rock,kholocs):
+    if(manual):
+        print(''.join(board))
+        print('0123456789abcd')
+        r_idx = int(input("Which Rock: "))
+        b_idx = int(input("Where to  : "))
+        return r_idx,b_idx
+        
+    r_idx = np.argwhere(board==rock).reshape(1,-1)[0]
+    b_idx = np.argwhere(board=='_').reshape(1,-1)[0]
+    #b_idx = np.delete(b_idx, np.isin(b_idx,kholocs))
+    return random.choice(r_idx), random.choice(b_idx)
+
+def detect_suicide(board, rock):
+    s = ''.join(board)
+    if (rock == 'x'):
+        kill = re.compile('(?=(xo+x))')
+        suicide = re.compile('(?=(ox+o))')
+    else:
+        kill = re.compile('(?=(ox+o))')
+        suicide = re.compile('(?=(xo+x))')
+
+    for m in re.finditer(kill,s):
+        board[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+    s = ''.join(board)
+    for m in re.finditer(suicide,s):
+        board[m.start(1)+1:m.end(1)-1] = (' '.join('_'*(len(m.group(1))-2))).split(' ')
+        return True
+
+    return False
+
+def pickle_rounds(r,g,rock):
+    o = np.array(g)
+    if(rock=='x'):
+        r = np.where(r=='x',1,r)
+        r = np.where(r=='o',-1,r)
+        r = np.where(r=='_',0,r)
+    else:
+        r = np.where(r=='o',1,r)
+        r = np.where(r=='x',-1,r)
+        r = np.where(r=='_',0,r)
+    r = np.array(r,dtype=float)
+
+
+    if Path("./alak_x_addon.pkl").is_file():
+        rounds = np.array([])
+        outcome = np.array([])
+        with open("./alak_x_addon.pkl","rb") as rfile:
+            rounds = pickle.load(rfile)
+            rounds = np.vstack((rounds,r))
+        with open("./alak_y_addon.pkl","rb") as rfile:
+            outcome = pickle.load(rfile)
+            outcome = np.concatenate((outcome,o))
+        with open("./alak_x_addon.pkl","wb") as wfile:
+            pickle.dump(rounds,wfile)
+        with open("./alak_y_addon.pkl","wb") as wfile:
+            pickle.dump(outcome,wfile)
+    else:
+        with open("./alak_x_addon.pkl","wb") as wfile:
+            pickle.dump(r,wfile)
+        with open("./alak_y_addon.pkl","wb") as wfile:
+            pickle.dump(o,wfile)
+    return
+
+
+
+if __name__ == "__main__":
+
+    # Toss
+    no_games = 100
+    ANN_vict_stat = []
+    ann_vict = 0
+    suicide_games = 0
+    deadlocks = 0
+    for games in range(no_games):
+        game_on = True
+        toss = np.random.randint(2)
+        if(toss):
+            nn_rock = 'x'
+            rand_rock = 'o'
+            #print(f'Game No {games}: NN won the toss.')
+        else:
+            nn_rock = 'o'
+            rand_rock = 'x'
+            #print(f'Game No {games}: Rand won the toss.')
+
+        # setup Board 
+        b = Board(14,toss)
+
+        nnmodel = NNClass(nn_rock,"perfect_model_90.pkl")
+        #nnmodel2 = NNClass(rand_rock,"perfect_model_80.pkl")
+
+        if(toss == 0):
+            game_on = b.play_n_validate(randplay,rand_rock)
+            if (not game_on):
+                raise Exception("Game cant be over in one move!!")
+
+
+        altfuncs =[nnmodel.nnplay,randplay]
+        rocks = [nn_rock,rand_rock]
+        i = 0
+        while (game_on):
+            game_on = b.play_n_validate(altfuncs[i%2],rocks[i%2])
+            i += 1
+
+        if(b.deadlock):
+            deadlocks += 1
+            print("Machines were in deadlock. Abandoning the game")
+            continue
+
+        if(i%2):
+            ANN_vict_stat.append([nn_rock,1,i+1])
+            ann_vict += 1
+            #if(not b.suicide_in_game):
+                #pickle_rounds(b.rounds,b.gain,nn_rock)
+            print(f'NN Won in {i} steps!!')
+        else:
+            ANN_vict_stat.append([nn_rock,0,i+1])
+            #if(not b.suicide_in_game):
+                #pickle_rounds(b.rounds,b.gain,rand_rock)
+            print(f'RANDPLAY Won in {i} steps!!')
+        if (b.suicide_in_game):
+            suicide_games += 1
+        print(f'total rounds {b.rounds.shape}')
+
+    print(np.array(ANN_vict_stat))
+    print(f'NN won {ann_vict} out of {no_games} games with {suicide_games} with suicide and {deadlocks} deadlocks')
---
+++ onestepmulti/test.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pickle
+import pandas as pd
+from pathlib import Path
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+ylabel = []
+X = []
+if Path("./alak_y.pkl").is_file():
+    print("File alak_y.pkl exists")
+    ylabel = pickle.load(open("./alak_y.pkl","rb"))
+    print(ylabel.shape,np.unique(ylabel))
+if Path("./alak_x.pkl").is_file():
+    print("File alak_x.pkl exists")
+    X = pickle.load(open("./alak_x.pkl","rb"))
+    print(X.shape,np.unique(X))
+else:
+    print("File Does not exist")
+if Path("./perfect_model.pkl").is_file():
+    model = pickle.load(open("./perfect_model.pkl","rb"))
+    print(type(model))
+
+print(ylabel.mean())
+
+#print(type(ylabel[0]))
+#ylabel = ylabel.astype(int)
+#print(type(ylabel[0]),np.unique(ylabel))
+#print(type(X[0][0]),np.unique(X))
+#X = np.where(X=='x','1',X)
+#print(type(X[0][0]),np.unique(X))
+#X = np.where(X=='_','0',X)
+#print(type(X[0][0]),np.unique(X))
+#X = np.where(X=='o','-1',X)
+#print(type(X[0][0]),np.unique(X))
+#X = X.astype(int)
+#print(type(X[0][0]),np.unique(X))
+
